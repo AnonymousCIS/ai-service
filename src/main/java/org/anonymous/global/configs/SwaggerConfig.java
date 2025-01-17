9043fd045276a65326375732a0b95a8c3552c58f
kks940104 <kks940104@gmail.com>
2025-01-17 16:23:33 +0900

    kks - 초기세팅
    kks - 초기세팅

--- src/main/java/org/anonymous/global/configs/SwaggerConfig.java
+++ src/main/java/org/anonymous/global/configs/SwaggerConfig.java
@@ -7,13 +7,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(info=@Info(title="파일 API"))
+@OpenAPIDefinition(info=@Info(title="API Service API"))
 public class SwaggerConfig {
     
     @Bean
     public GroupedOpenApi apiGroup() {
         return GroupedOpenApi.builder()
-                .group("파일 API v1")
+                .group("API Service API v1")
                 .pathsToMatch("/**")
                 .build();
     }
